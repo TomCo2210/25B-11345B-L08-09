dc429b9ceffef7c3c7ee94d91bd21ed7acbe6bb5
TomCo2210 <tomco@outlook.co.il>
2025-05-21 20:33:36 +0300

    Initial commit

---
+++ app/src/main/java/dev/tomco/a25b_11345b_l08_09/MainActivity.kt
@@ -0,0 +1,29 @@
+package dev.tomco.a25b_11345b_l08_09
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import dev.tomco.a25b_11345b_l08_09.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
